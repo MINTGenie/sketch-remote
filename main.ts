3f2e93cc7152f7171faa289a0bae971eaeb430e5
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-12-04 03:09:24 +0100

    added feature to plot on led

--- main.ts
+++ main.ts
@@ -2,30 +2,60 @@ enum RadioMessage {
     message1 = 49434
 }
 pins.onPulsed(DigitalPin.P16, PulseValue.High, function () {
-    coloridx += 1
-    if (coloridx > 9) {
-        coloridx = 0
+    if (!(coloridx)) {
+        coloridx = true
+        radio.sendValue("coloridx", 1)
     }
-    radio.sendValue("coloridx", coloridx)
 })
 pins.onPulsed(DigitalPin.P15, PulseValue.High, function () {
     show = 1
+})
+pins.onPulsed(DigitalPin.P16, PulseValue.Low, function () {
+    coloridx = false
 })
 pins.onPulsed(DigitalPin.P15, PulseValue.Low, function () {
     show = 0
     radio.sendValue("draw", 1)
 })
+let prevY = 0
+let prevX = 0
+let tmpY = 0
+let tmpX = 0
 let show = 0
-let coloridx = 0
+let coloridx = false
 radio.setGroup(1)
 serial.writeValue("x", 0)
 serial.writeNumbers([pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2)])
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-coloridx = 0
+coloridx = false
 basic.forever(function () {
-    if (show) {
-        radio.sendValue("10000x+y", pins.analogReadPin(AnalogPin.P1) * 10000 + pins.analogReadPin(AnalogPin.P2))
-        basic.pause(50)
+    tmpX = pins.map(
+    pins.analogReadPin(AnalogPin.P2),
+    0,
+    1023,
+    7,
+    0
+    )
+    tmpY = pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    0,
+    7
+    )
+    if (!(prevX == tmpX && prevY == tmpY)) {
+        if (show) {
+            radio.sendValue("10000x+y", pins.analogReadPin(AnalogPin.P1) * 10000 + pins.analogReadPin(AnalogPin.P2))
+            basic.pause(50)
+        } else {
+            radio.sendValue("cursor", pins.analogReadPin(AnalogPin.P1) * 10000 + pins.analogReadPin(AnalogPin.P2))
+            led.unplot(prevX, prevY)
+            led.plot(tmpX, tmpY)
+            basic.pause(50)
+        }
+        prevX = tmpX
+        prevY = tmpY
     }
+    basic.pause(50)
 })
